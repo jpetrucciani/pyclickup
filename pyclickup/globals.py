863a9f975741a22a8a88fdcfa7b35b683e50f6c4
Jacobi Petrucciani <j@mimir.cloud>
2018-09-17 15:55:47 -0400

    [DevOps]: fixing the packaging issue

--- pyclickup/globals.py
+++ pyclickup/globals.py
@@ -3,7 +3,7 @@ globals
 """
 
 
-__version__ = "0.0.2"
+__version__ = "0.0.3"
 
 
 LIBRARY = "pyclickup"
--- setup.py
+++ setup.py
@@ -3,19 +3,25 @@
 pip setup file
 """
 from pyclickup.globals import __version__, LIBRARY
-from setuptools import setup
+from setuptools import setup, find_packages
+
+
+with open("README.md") as readme:
+    long_description = readme.read()
 
 
 setup(
     name=LIBRARY,
     version=__version__,
-    description=("A python wrapper for the ClickUp API"),
+    description="A python wrapper for the ClickUp API",
+    long_description=long_description,
     author="Jacobi Petrucciani",
     author_email="jacobi@mimirhq.com",
+    keywords="clickup python",
     url="https://github.com/jpetrucciani/{}.git".format(LIBRARY),
     download_url="https://github.com/jpetrucciani/{}.git".format(LIBRARY),
     license="LICENSE",
-    packages=[LIBRARY, "{}.models".format(LIBRARY)],
+    packages=find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.4",
@@ -23,4 +29,5 @@ setup(
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
     ],
+    zip_safe=False,
 )
