30ad208a3ead6fe179f752adb4f3cceb207d37aa
Jacobi Petrucciani <8117202+jpetrucciani@users.noreply.github.com>
2018-11-29 10:37:46 -0800

    fixing the test api url
    the apiary url had changed, causing tests to fail

--- pyclickup/globals.py
+++ pyclickup/globals.py
@@ -10,7 +10,7 @@ LIBRARY = "pyclickup"
 API_URL = "https://api.clickup.com/api/v1/"
 
 
-TEST_API_URL = "https://private-anon-fc354a5c9f-clickup.apiary-mock.com/api/v1/"
+TEST_API_URL = "https://private-anon-8c786c6f10-clickup.apiary-mock.com/api/v1/"
 TEST_TOKEN = "access_token"
 
 
